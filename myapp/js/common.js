4d5df7a6882c7c0c64f70b6472ffe265c9d4dc7c
Lia <liaforweb@gmail.com>
2020-03-10 16:10:48 +0200

    font

--- myapp/js/common.js
+++ myapp/js/common.js
@@ -228,43 +228,4 @@ $(function() {
 
 
 
-document.addEventListener('DOMContentLoaded', function(){
-//menu
-    var menu = document.querySelector('.menu-toggle');
-    menu.addEventListener('click', function(){
-        var nav = document.querySelector('.main-menu');
-        nav.classList.toggle('active');
-        var navGamb = document.querySelector('.menu-toggle');
-        navGamb.classList.toggle('active');
-    });
-//tabs
-	// store tabs variable
-	var myTabs = document.querySelectorAll("ul.header__tabs > li");
-    function myTabClicks(tabClickEvent) {
-		for (var i = 0; i < myTabs.length; i++) {
-			myTabs[i].classList.remove("active");
-		}
-		var clickedTab = tabClickEvent.currentTarget;
-		clickedTab.classList.add("active");
-		tabClickEvent.preventDefault();
-		var myContentPanes = document.querySelectorAll(".tab-pane");
-		for (i = 0; i < myContentPanes.length; i++) {
-			myContentPanes[i].classList.remove("active");
-		}
-        var anchorReference = tabClickEvent.target;
-        console.log(anchorReference);
-        var activePaneId = anchorReference.getAttribute("href");
-        console.log(activePaneId);
-        var activePane = document.querySelector(activePaneId);
-        console.log(activePaneId);
-		activePane.classList.add("active");
-    }
-    for (i = 0; i < myTabs.length; i++) {
-		myTabs[i].addEventListener("click", myTabClicks)
-	}
 
-
-
-
-
-});
